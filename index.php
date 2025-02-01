f0b24e7368625746534f2b1e6f282790efb5046e
bizzaro <Bizzaro@users.noreply.github.com>
2025-02-01 17:06:16 -0500

    refactor main

--- index.php
+++ index.php
@@ -2,88 +2,107 @@
 date_default_timezone_set('UTC');
 include("lib/can-full-size.php");
 
-$CHK = new CheckGenerator;
-
-function setBankLogo(&$check)
+class Check
 {
-  // Define a mapping of transit numbers to logo file paths
-  $logoMap = [
-    "001" => "logos/bmo.png",
-    "002" => "logos/bns.png",
-    "003" => "logos/rbc.png",
-    "004" => "logos/td.png",
-    "006" => "logos/nbc.png",
-    "010" => "logos/cibc.png"
-  ];
+    private $logoMap = [
+        "001" => "logos/bmo.png",
+        "002" => "logos/bns.png",
+        "003" => "logos/rbc.png",
+        "004" => "logos/td.png",
+        "006" => "logos/nbc.png",
+        "010" => "logos/cibc.png"
+    ];
 
-  // Log the institution number for debugging
-  error_log("inst number: " . $check['inst_number']);
+    private $checkData = [];
+    private $defaultData = [];
 
-  // Check if the institution number exists in the mapping
-  if (isset($check['inst_number']) && array_key_exists($check['inst_number'], $logoMap)) {
-    $check['bank_logo'] = $logoMap[$check['inst_number']];
-  } else {
-    error_log('Nothing found');
-    // Set a default logo if the inst number is not found
-    $check['bank_logo'] = "";
-  }
+    public function __construct($data, $defaultData = [])
+    {
+        $this->defaultData = $defaultData;
+        $this->checkData = array_merge($this->defaultData, $data);
+        $this->setBankLogo();
+    }
+
+    public function set($key, $value)
+    {
+        $this->checkData[$key] = $value;
+    }
+
+    public function get($key)
+    {
+        return $this->checkData[$key] ?? null;
+    }
+
+    public function getAll()
+    {
+        return $this->checkData;
+    }
+
+    private function setBankLogo()
+    {
+        error_log("inst number: " . ($this->checkData['inst_number'] ?? 'N/A'));
+        if (!empty($this->checkData['inst_number']) && array_key_exists($this->checkData['inst_number'], $this->logoMap)) {
+            $this->checkData['bank_logo'] = $this->logoMap[$this->checkData['inst_number']];
+        } else {
+            error_log('Nothing found for institution number');
+            $this->checkData['bank_logo'] = "";
+        }
+    }
 }
 
-$check['inst_number'] = "002";
-setBankLogo($check);
+$CHK = new CheckGenerator;
 
-$check['my_logo'] = "";
-$check['from_name'] = "Your Name";
+$defaultCheckData = [
+    'inst_number' => "002",
+    'my_logo' => "",
+    'from_name' => "Your Name",
+    'from_address1' => "234 Bay Street",
+    'from_address2' => "Toronto, ON M5K 1B2",
+    'transit_number' => "12345",
+    'account_number' => "123-456-789",
+    'bank_1' => "Bank of Canada",
+    'bank_2' => "234 Wellington Street",
+    'bank_3' => "Ottawa, ON K1A 0G9",
+    'bank_4' => "",
+    'signature' => "./signatures/thor.png",
+    'pay_to' => "YOUR MOM",
+    'amount' => '9999999.99',
+    'date' => "2020-01-01",
+    'memo' => "TEST",
+    'check_number' => 1000
+];
 
-$check['from_address1'] = "234 Bay Street";
-$check['from_address2'] = "Toronto, ON M5K 1B2";
+$customCheckData1 = [
+    'transit_number' => '9999999',
+    'pay_to' => "John Doe",
+    'amount' => '5000.00',
+    'memo' => "Salary Payment"
+];
 
-$check['transit_number'] = "12345";
-// include dashes in your account number
-// BMO - 123456-123
-// BNS - 12345-12
-// RBC - 123-123-1
-// TD - 1234-1234567
-// NBC - 12-123-12
-// CIBC - 12-12345
-// DESJ - 123-123-1
-// CU - 12345678-123
-$check['account_number'] = "123-456-789";
+$check1 = new Check($customCheckData1, $defaultCheckData);
+$CHK->AddCheck($check1->getAll());
 
-$check['bank_1'] = "Bank of Canada";
-$check['bank_2'] = "234 Wellington Street";
-$check['bank_3'] = "Ottawa, ON K1A 0G9";
-$check['bank_4'] = "";
+$customCheckData2 = [
+    'check_number' => $check1->get('check_number') + 1,
+    'pay_to' => "Jane Smith",
+    'amount' => '7500.00'
+];
 
-$check['signature'] = "./signatures/thor.png";
-// $check['signature'] = "Albert";
+$check2 = new Check($customCheckData2, $defaultCheckData);
+$CHK->AddCheck($check2->getAll());
 
-$check['pay_to'] = "YOUR MOM";
-$check['amount'] = '9999999.99';
-$check['date'] = "2020-01-01";
-$check['memo'] = "TEST";
 
-// 3 checks per page
-
-$check['check_number'] = 1000;
-$CHK->AddCheck($check);
-
-$check['check_number']++;
-$CHK->AddCheck($check);
-
-$check['check_number']++;
-$CHK->AddCheck($check);
+$check3 = new Check($customCheckData2, $defaultCheckData);
+$CHK->AddCheck($check3->getAll());
 
 if (array_key_exists('REMOTE_ADDR', $_SERVER)) {
-  // Called from a browser
-  header('Content-Type: application/octet-stream', false);
-  header('Content-Type: application/pdf', false);
-  $CHK->PrintChecks();
+    header('Content-Type: application/octet-stream', false);
+    header('Content-Type: application/pdf', false);
+    $CHK->PrintChecks();
 } else {
-  // Called from the command line
-  ob_start();
-  $CHK->PrintChecks();
-  $pdf = ob_get_clean();
-  file_put_contents('checks.pdf', $pdf);
-  echo "Saved to file: checks.pdf\n";
+    ob_start();
+    $CHK->PrintChecks();
+    $pdf = ob_get_clean();
+    file_put_contents('checks.pdf', $pdf);
+    echo "Saved to file: checks.pdf\n";
 }
